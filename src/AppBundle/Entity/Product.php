3cdcd1e033f8c23bdfad973a2dc038d2a7313315
DESKTOP-UNI4IPS\Jschouwstra <jelleschouwstra@heinenoord.nl>
2017-02-22 11:16:51 +0100

    pm-5 test

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -12,13 +12,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Product
 {
-
-    /**
-     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
-     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
-     */
-    private $category;
-    
     /**
      * @var int
      *
@@ -69,28 +62,5 @@ class Product
     {
         return $this->name;
     }
-
-    /**
-     * Set category
-     *
-     * @param \AppBundle\Entity\Category $category
-     *
-     * @return Product
-     */
-    public function setCategory(\AppBundle\Entity\Category $category = null)
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
-    /**
-     * Get category
-     *
-     * @return \AppBundle\Entity\Category
-     */
-    public function getCategory()
-    {
-        return $this->category;
-    }
 }
+
