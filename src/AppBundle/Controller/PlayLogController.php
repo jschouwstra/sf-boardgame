57a6303aa5c74d640084f982311cc121ed606044
DESKTOP-UNI4IPS\Jschouwstra <jelleschouwstra@heinenoord.nl>
2017-03-17 11:22:45 +0100

    pm 11 basic crud playlog

---
+++ src/AppBundle/Controller/PlayLogController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\PlayLog;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Playlog controller.
+ *
+ * @Route("playlog")
+ */
+class PlayLogController extends Controller
+{
+    /**
+     * Lists all playLog entities.
+     *
+     * @Route("/", name="playlog_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $playLogs = $em->getRepository('AppBundle:PlayLog')->findAll();
+
+        return $this->render('playlog/index.html.twig', array(
+            'playLogs' => $playLogs,
+        ));
+    }
+
+    /**
+     * Creates a new playLog entity.
+     *
+     * @Route("/new", name="playlog_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $playLog = new Playlog();
+        $form = $this->createForm('AppBundle\Form\PlayLogType', $playLog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($playLog);
+            $em->flush($playLog);
+
+            return $this->redirectToRoute('playlog_show', array('id' => $playLog->getId()));
+        }
+
+        return $this->render('playlog/new.html.twig', array(
+            'playLog' => $playLog,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a playLog entity.
+     *
+     * @Route("/{id}", name="playlog_show")
+     * @Method("GET")
+     */
+    public function showAction(PlayLog $playLog)
+    {
+        $deleteForm = $this->createDeleteForm($playLog);
+
+        return $this->render('playlog/show.html.twig', array(
+            'playLog' => $playLog,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing playLog entity.
+     *
+     * @Route("/{id}/edit", name="playlog_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, PlayLog $playLog)
+    {
+        $deleteForm = $this->createDeleteForm($playLog);
+        $editForm = $this->createForm('AppBundle\Form\PlayLogType', $playLog);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('playlog_edit', array('id' => $playLog->getId()));
+        }
+
+        return $this->render('playlog/edit.html.twig', array(
+            'playLog' => $playLog,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a playLog entity.
+     *
+     * @Route("/{id}", name="playlog_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, PlayLog $playLog)
+    {
+        $form = $this->createDeleteForm($playLog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($playLog);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('playlog_index');
+    }
+
+    /**
+     * Creates a form to delete a playLog entity.
+     *
+     * @param PlayLog $playLog The playLog entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(PlayLog $playLog)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('playlog_delete', array('id' => $playLog->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/PlayLog.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PlayLog
+ *
+ * @ORM\Table(name="play_log")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlayLogRepository")
+ */
+class PlayLog
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="game_id", type="integer")
+     */
+    private $gameId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return PlayLog
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set gameId
+     *
+     * @param integer $gameId
+     *
+     * @return PlayLog
+     */
+    public function setGameId($gameId)
+    {
+        $this->gameId = $gameId;
+
+        return $this;
+    }
+
+    /**
+     * Get gameId
+     *
+     * @return int
+     */
+    public function getGameId()
+    {
+        return $this->gameId;
+    }
+}
+
