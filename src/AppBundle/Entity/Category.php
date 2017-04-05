ae3c3a2ab9f8c4ffb8f47a7fd3facd375a019984
Jelle Schouwstra <jelleschouwstra@heinenoord.nl>
2017-04-05 18:00:16 +0200

    no message

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Entity;
+namespace AppBundle\Entity\Game;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -9,10 +10,21 @@ use Doctrine\Common\Collections\ArrayCollection;
  *
  * @ORM\Table(name="category")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
- */
+ *
+ *
+**/
+
+
 class Category
 {
 
+/**
+ * @ManyToMany(targetEntity="Game")
+ * @JoinTable(name="category_game",
+ *      joinColumns={@ORM\JoinColumn(name="category_id", referencedColumnName="id")},
+ *      inverseJoinColumns={@ORM\JoinColumn(name="game_id", referencedColumnName="id")}
+ *      )
+ */
     private $games;
 
     public function __construct()
