e3d89b3c13da66a85e4f9f19ef587379fb692c57
Jelle Schouwstra <jelleschouwstra@heinenoord.nl>
2017-10-31 16:01:50 +0100

    - added text limit - expansion list bullets - Game entitiy organized

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,7 +1,16 @@
 /**
  * Created by Jelle on 19-6-2017.
  */
-function goBack() {
-    window.history.go(-1);
-}
+$(document).ready(function() {
 
+    function goBack() {
+        window.history.go(-1);
+    }
+
+    $(".text-limit").each(function () {
+        var maxLength = 47;
+        if ($(this).text().length > maxLength) {
+            $(this).text($(this).text().substr(0, maxLength) + "...");
+        }
+    });
+});
